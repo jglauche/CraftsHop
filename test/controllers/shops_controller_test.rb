abdba7063863c606f19f806efa9a2c2934554e76
Jennifer Glauche <mail@jglauche.de>
2020-06-30 17:56:49 +0200

    make shop controller name plural

--- test/controllers/shops_controller_test.rb
+++ test/controllers/shops_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class ShopControllerTest < ActionDispatch::IntegrationTest
+class ShopsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
